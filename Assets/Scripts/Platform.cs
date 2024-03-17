70805c6022d13dbb7dcabb5998da6fde1c81a6b4
Nounce <91398230+NounceHD@users.noreply.github.com>
2024-03-17 15:17:44 -0400

    Moving platforms and landscape added

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] bool moving = false;
+    [SerializeField] private Vector3 finishPos = Vector3.zero;
+    [SerializeField] private float speed = 0.5f;
+
+    private Vector3 startPos;
+    private float trackPercent = 0f;
+    private int direction = 1;
+
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (moving)
+        {
+            trackPercent += direction * speed * Time.deltaTime;
+
+            float x = finishPos.x * trackPercent + startPos.x;
+            float y = finishPos.y * trackPercent + startPos.y;
+            transform.position = new Vector3(x, y, startPos.z);
+
+            if ((direction == 1 & trackPercent > 0.9f) || (direction == -1 && trackPercent < 0.1f))
+            {
+                direction *= -1;
+            }
+        }
     }
 }
